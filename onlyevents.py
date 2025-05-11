e931f1cf3b59f45bc92c6adc656e633b02cb8a91
adl1703 <wsx001@protonmail.com>
2025-05-11 17:08:54 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -10,8 +10,8 @@ from bs4 import BeautifulSoup
 import time
 # Costanti
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://asciughino-omg-tv.hf.space"
+PASSWORD = "api"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
