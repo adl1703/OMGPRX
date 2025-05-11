9a57ab8d9127471862bb85d7a7f073013f08c6ad
adl1703 <wsx001@protonmail.com>
2025-05-11 17:07:59 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -12,8 +12,8 @@ import time
 
 # Constants
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://asciughino-omg-tv.hf.space"
+PASSWORD = "api"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
